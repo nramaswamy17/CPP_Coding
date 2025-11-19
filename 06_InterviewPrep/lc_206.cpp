b69946c80c7e5645489160be7e39dfa2b07538d5
Neal Ramaswamy <nramaswamy17@gmail.com>
2025-11-18 17:14:04 -0800

    Completed LC 206

--- 06_InterviewPrep/lc_206.cpp
+++ 06_InterviewPrep/lc_206.cpp
@@ -14,3 +14,44 @@ Idea:
     (1) => (2) => (3)
     (1) <= (2) <= (3)
 */
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+Node* reverseList(Node* head) {
+    Node* prev = nullptr;
+    Node* curr = head;
+
+    while(curr != nullptr) {
+        Node* next = curr->next;
+
+        curr->next = prev; // points to previous now
+        prev = curr; // move prev now to current ptr
+        curr = next; // move current ptr to next to reset the state
+    }
+
+    return prev;
+}
+
+int main () {
+
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+
+    head = reverseList(head);
+
+    cout << head->data<< " " << head->next->data << " " << head->next->next->data;
+
+    return 0;
+}
